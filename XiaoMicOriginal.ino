011a4e94ab95c9446a1c9d87f60e09ee60460296
Omar El Sayed <108880086+oelsayed10@users.noreply.github.com>
2022-08-04 13:09:31 -0400

    Update XiaoMicOriginal.ino

--- XiaoMicOriginal.ino
+++ XiaoMicOriginal.ino
@@ -3,7 +3,10 @@
 #include "RunningAverage.h"
 
 #define SAMPLES 256             //Must be a power of 2
-#define SAMPLING_FREQUENCY 16000 //Hz, must be less than 10000 due to ADC
+#define SAMPLING_FREQUENCY 12000 //Hz, must be less than 10000 due to ADC
+#define samp_siz 4 // Experiment
+#define rise_threshold 4 // Experiment 
+
 short sampleBuffer[SAMPLES];
 volatile int samplesRead;
 
@@ -12,7 +15,19 @@ unsigned long microseconds;
 
 RunningAverage myRA(10); //include these 2 lines of code before void setup()
 int samples = 0;
+int count = 0; 
+int data; 
+bool inPeak = false;
 
+int x = 0;
+int LastTime = 0;
+bool BPMTiming = false;
+bool BeatComplete = false;
+int BPM = 0;    
+#define UpperThreshold 2500
+#define LowerThreshold 1700 
+int LED13 = 44; // The on-board Arduino LED
+int Signal; // holds the incoming raw data. Signal value can range from 0-1024
 
 double vReal[SAMPLES];
 double vImag[SAMPLES];
@@ -43,6 +58,8 @@ void setup() {
   Serial.print("Version: ");
   Serial.println(RUNNINGAVERAGE_LIB_VERSION);
   myRA.clear(); // explicitly start clean -- ONLY need this line in void setup()
+  
+  Serial.println("heart rate: "); // Experiment
 }
 
 void loop() {
@@ -65,16 +82,42 @@ void loop() {
   Serial.print("Running Average: ");
   Serial.println(myRA.getAverage(), 4);
 
-  if (samples == 300) // this line sets the number of samples included in the running average
+  if (samples == 4000) // this line sets the number of samples included in the running average
   {
     samples = 0;
     myRA.clear();
-  }
-
-
-    
+  } 
     samplesRead = 0;
-   
+    //delay(80); 
+    
+int value = myRA.getAverage();
+  if (value > UpperThreshold) {
+    if (BeatComplete) {
+      BPM = millis() - LastTime;
+      BPM = int(60 / (float(BPM) / 1000));
+      BPMTiming = false;
+      BeatComplete = false;
+    }
+    if (BPMTiming == false) {
+      LastTime = millis();
+      BPMTiming = true;
+    }
+  }
+  if ((value < LowerThreshold) & (BPMTiming))
+    BeatComplete = true;
+    // display bpm
+  Serial.print(BPM);
+  Serial.println(" BPM");
+  x++;
+  Signal = myRA.getAverage(); // Read the PulseSensor's value.
+  // Assign this value to the "Signal" variable.
+  if (Signal > UpperThreshold) {
+    // If the signal is above "550", then "turn-on" Arduino's on-Board LED.
+    digitalWrite(LED13, HIGH);
+  } else {
+    digitalWrite(LED13, LOW); //  Else, the signal must be below "550", so "turn-off" this LED.
+  }
+    
   }
 }
 
