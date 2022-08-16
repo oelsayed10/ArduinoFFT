dc9ec2f5b55951b395ee7948d6aef2e9408e693e
Omar El Sayed <108880086+oelsayed10@users.noreply.github.com>
2022-08-16 14:03:41 -0400

    Update NEW_BPM_Monitor.ino

--- NEW_BPM_Monitor.ino
+++ NEW_BPM_Monitor.ino
@@ -4,6 +4,27 @@
 #include "RunningAverage.h"
 #include "ArduinoBLE.h"  
 
+
+#define SAMPLES 256             //Must be a power of 2
+#define SAMPLING_FREQUENCY 16000 //Hz, must be less than 10000 due to ADC
+short sampleBuffer[SAMPLES];
+volatile int samplesRead;
+
+unsigned long microseconds;
+
+
+double vReal[SAMPLES];
+double vImag[SAMPLES];
+
+void onPDMdata(void);
+
+const uint8_t amplitude = 100;
+
+arduinoFFT FFT = arduinoFFT();
+
+RunningAverage myRA(10); //for the running average
+int samples = 0;
+
 int UpperThreshold = 2500;
 int LowerThreshold = 1700;
 int reading = 0;
@@ -23,17 +44,66 @@ void setup(){
   Serial.begin(baudRate);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
+
+   myRA.clear(); 
+
+    PDM.onReceive(onPDMdata);
+  PDM.setBufferSize(SAMPLES);
+  //PDM.setGain(0);
+  // setup the I2S audio input for the sample rate with 32-bits per sample
+  if (!PDM.begin(1, 16000)) {
+    Serial.println("Failed to start PDM!");
+    while (1);
+  }
 }
 
 void loop(){
 
+ if (samplesRead) {
+    for (int i = 0; i < SAMPLES; i++) {
+      vReal[i] = sampleBuffer[i];
+      vImag[i] = 0;
+    }
+
+    FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+    FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+    FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+ 
+    double peak = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
+    
+  //Serial.println(peak);   //prints all frequency values, even background noise and voices
+    
+    samplesRead = 0;
+
+   if (peak < 1120){     //gets rid of some background noise and voices, prints 0 instead of the actual frequency
+      Serial.println(0);
+    }
+    else {
+      Serial.println(peak);
+    }
+    
+    
+    double rn = peak;
+    myRA.addValue(rn);
+    samples++;
+    Serial.print("Running Average: ");
+    Serial.println(myRA.getAverage(), 4); //prints the running average after each frequency that is printed
+
+    if (samples == 4000)
+    {
+     samples = 0;
+      myRA.clear();
+    }
+    samplesRead = 0;
+     //delay(100);
+     
   // Get current time
   unsigned long currentMillis = millis();
 
   // First event
   if(myTimer1(delayTime, currentMillis) == 1){
 
-    reading = analogRead(0); 
+    reading = myRA.getAverage(); 
 
     // Heart beat leading edge detected.
     if(reading > UpperThreshold && IgnoreReading == false){
@@ -71,6 +141,7 @@ void loop(){
     Serial.flush();
   }
 }
+}
 
 // First event timer
 int myTimer1(long delayTime, long currentMillis){
@@ -82,4 +153,12 @@ int myTimer1(long delayTime, long currentMillis){
 int myTimer2(long delayTime2, long currentMillis){
   if(currentMillis - previousMillis2 >= delayTime2){previousMillis2 = currentMillis;return 1;}
   else{return 0;}
+}
+
+void onPDMdata()
+{
+  int bytesAvailable = PDM.available();
+  PDM.read(sampleBuffer, bytesAvailable);
+  samplesRead = bytesAvailable / 2;
+
 }
